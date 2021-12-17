d30c78aef495ceb48b4bba73d3f25422df3de990
yuanxiuxiang <1050251488@qq.com>
2021-12-17 16:52:13 +0800

    登录界面

--- project-java-script-meizu-master/src/js/login.js
+++ project-java-script-meizu-master/src/js/login.js
@@ -1,3 +1,49 @@
 //获取元素
-let user = document.getElementById('user')
-let password = document.getElementById('password')
+let submit = document.getElementById('submit');//登录
+let user = document.getElementById('user');//用户名
+let password = document.getElementById('password');//密码
+let hb1 = document.getElementById('helpBlock1');
+let hb2 = document.getElementById('helpBlock2');
+let xmlHttp = new XMLHttpRequest();
+let jsonObj = null;
+xmlHttp.onload = function(){
+    if(xmlHttp.readyState == 4){
+        if(xmlHttp.status == 200){
+            txt = xmlHttp.responseText;
+            jsonObj = JSON.parse(txt);
+            console.info(jsonObj);
+        }else{
+            console.info('数据返回失败！状态代码：'+xmlHttp.status+'状态信息：'+xmlHttp.statusText);
+        }
+    }
+
+}
+xmlHttp.open('GET','http://localhost:3000/usermanager',true);
+xmlHttp.send();
+user.onblur = function(){
+    if(!user.value){
+        hb1.style.display = 'block';
+    }else{
+        hb1.style.display = 'none';
+    }
+}
+password.onblur = function(){
+    if(!password.value){
+        hb2.style.display = 'block';
+    }else{
+        hb2.style.display = 'none';
+    }
+}
+submit.onclick = function(){
+    let uname = user.value;
+    let pwd = password.value;
+    if(!uname){
+    }else if(!pwd){
+    }else{
+        for(let i = 0; i < jsonObj.length; i++){
+            if(uname==jsonObj[i]['name']&&pwd==jsonObj[i]['psw']){
+                console.log(jsonObj[i]);
+            }
+        }
+    }
+}
---
+++ project-java-script-meizu-master/src/js/register.js
@@ -0,0 +1,4 @@
+let user = document.getElementById('user');
+let password1 = document.getElementById('password1');
+let password2 = document.getElementById('password2');
+
